59da879d3033e497eb3ed35fa8fd14af3ef4b02b
Sophia Deato <sdeato011@gmail.com>
2024-05-17 18:14:33 +0800

    finished 3 screens design

---
+++ app/src/main/java/com/example/tcg/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tcg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.custom_timer);
+    }
+}
