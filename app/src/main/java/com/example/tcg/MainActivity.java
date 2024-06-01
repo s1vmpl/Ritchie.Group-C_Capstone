5a35196aeb8a322d3dd87cc1049f5ff4239a5c8a
alexy <alexyreaper@gmail.com>
2024-06-01 15:15:09 +0800

    i added the code

--- app/src/main/java/com/example/tcg/MainActivity.java
+++ app/src/main/java/com/example/tcg/MainActivity.java
@@ -1,14 +1,44 @@
-package com.example.tcg;
+package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.tcg.R;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+    TextView clock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.custom_timer);
+        setContentView(R.layout.health_tracker);
+
+        clock = findViewById(R.id.textClock);
+
+        long duration = TimeUnit.MINUTES.toMillis(30);
+
+        new CountDownTimer(duration, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                long minutes = TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished);
+                long seconds = TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(minutes);
+
+                String sDuration = String.format(Locale.ENGLISH, "%02d : %02d", minutes, seconds);
+
+                clock.setText(sDuration);
+            }
+
+            @Override
+            public void onFinish() {
+                clock.setText("game over");
+            }
+        }.start();
     }
 }
